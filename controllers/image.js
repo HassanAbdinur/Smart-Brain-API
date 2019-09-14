ba50e6ce3e8bebeb44960357695b526d64a49189
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-14 02:56:52 -0700

    rearranging code

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: `${process.env.REACT_APP_CLARIFAI_API_KEY}`
+   });
+
+   const handleApiCall = (req, req) => {
+       app.models
+      .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+      .then(data => {
+          res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+   }
+   
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,7 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleApiCall
+
 };

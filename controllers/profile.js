94caead992f4b29dee715853a83549620373ab1a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-24 18:06:30 -0700

    working on bugs

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,8 +6,7 @@ const handleProfileGet = (req, res, db) => {
             res.json(user[0])
         } else {
             res.status(400).json('Not Found')
-        }
-        
+        } 
     })
     .catch(err => res.status(400).json('error getting user'))
 }

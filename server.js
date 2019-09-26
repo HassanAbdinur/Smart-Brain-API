01217551d61923eb0d394c2eeb88871fbc719ab5
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-25 17:41:35 -0700

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-crystalline-93624',
       user : 'postgres',
       password : 'microarchitect',
       database : 'smart_brain'
